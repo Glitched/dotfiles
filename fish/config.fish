9803e091161f3cae3dc4b07a8542603020072900
Ryan Slama <rockinryguy@gmail.com>
2020-03-15 10:24:32 -0400

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,29 @@
+# Autoinstall fisher as a package manager
+# and install packages
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
 
+# Starship is a super cool prompt
 starship init fish | source
 
+# Editing
 alias vi="nvim"
+
+# Git
+alias g="git"
 alias gs="git status --short"
+
+# Quick directory navigation
 alias ...="cd ../../"
 alias ....="cd ../../../"
+alias .....="cd ../../../../"
 
+# Vi for all the things
 fish_vi_key_bindings
 
 # opam configuration
-source /Users/ryan/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
