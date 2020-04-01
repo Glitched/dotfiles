6753847d9f6def5604b5ff2be918bb4b63eb0d5c
Ryan Slama <rockinryguy@gmail.com>
2020-04-01 14:32:13 -0400

    Add lf setup

---
+++ lf/previewer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# lf does not support 24-bit colors
+unset COLORTERM
+
+# List files in archives
+# Send the rest to bat
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) unzip -l "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+    *.pdf) pdftotext "$1" -;;
+    *) bat --color always "$1" ;;
+esac
